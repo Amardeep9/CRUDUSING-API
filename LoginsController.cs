1e6668ad1ff277b675d26bbd3b3a584a54740dba
Rawat Amardeep ArjunPrasad <47588491+Amardeep9@users.noreply.github.com>
2019-07-22 14:54:25 +0530

    Add files via upload

---
+++ LoginsController.cs
@@ -0,0 +1,49 @@
+﻿using CRUDINWEBAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CRUDINWEBAPI.Controllers
+{
+    public class LoginsController : ApiController
+    {
+        angularEntities OE = new angularEntities();
+        public IQueryable<login> Get()
+        {
+            return OE.logins; // @@@@AFTER THIS METHOD ADD REFERENCE
+        }
+        //....api/Departments/1
+        public login Get(int id)
+        {
+            login login = OE.logins.Find(id);
+            return login;
+        }
+
+        //....api/Departments/1
+        public void Put(int id, login log)
+        {
+            OE.Entry(log).State = EntityState.Modified;
+            OE.SaveChanges();
+        }
+
+        //....api/Departments/1
+        public void Delete(int id)
+        {
+            login log = OE.logins.Find(id);
+            OE.logins.Remove(log);
+            OE.SaveChanges();
+        }
+
+        //....api/Departments/
+        public void Post(login log)
+        {
+            OE.logins.Add(log);
+            OE.SaveChanges();
+        }
+
+    }
+}
